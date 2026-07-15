0cecd2d77cdbe27ac76470ee5b1ac5c95a7092f5
Soumyadip Dey <kutidey00677@gmail.com>
2026-07-15 14:15:07 +0530

    Add files via upload

---
+++ basic c.c
@@ -0,0 +1,49 @@
+   #include <stdio.h>
+   /*
+    void main()
+    {
+        int a = 3;
+        int b = ++a + a++ + --a;
+        printf("Value of b is %d", b);
+    }
+
+    #Celcius to fehrenheit
+
+ #include <stdio.h>
+int main()
+{
+    int c,f;
+    printf("Enter the temperature in centigrade: ");
+    scanf("%d",&c);
+    f=(9*c/5)+32;
+    printf("temperature in fehrenheit is: %d", f);
+
+    return 0;
+}
+    
+    area of a circle    
+
+   #include <stdio.h>
+    int main()
+    {
+        float pi=3.14;
+        float r;
+        printf("Enter the radius of circle: ");
+        scanf("%f",&r);
+        float a=2*pi*r;
+        printf("Area of circle is: %f", a);
+        return 0;
+    }*/
+
+int main()
+{
+ int a=1; int b = a;
+ int v = 3*3;
+ char dt = '21 dec 2020';
+ printf("value of a is %d\n",a);
+ printf("value of b is %d\n",b);    
+ printf("value of v is %d\n",v);
+ printf("date is %c\n",dt); 
+ 
+ return 0;
+}
